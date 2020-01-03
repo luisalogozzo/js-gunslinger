020a413ddb276a87d3e36a7a069f61eca474f375
Luisa Logozzo <luisa.logozzo@gmail.com>
2020-01-03 19:31:01 +0100

    Update

---
+++ js/script.js
@@ -0,0 +1,23 @@
+document.getElementById('gioca').addEventListener('click',
+function() {
+  // document.getElementById('sipariodx').classList.add('sipario-dx');
+  // document.getElementById('sipariosx').classList.add('sipario-sx');
+  document.getElementById('gunman2').classList.add('rotate');
+
+  // document.getElementById('air1').classList.add('animazione-sx');
+  // document.getElementById('eagle').classList.add('animazione-sx');
+  // document.getElementById('gunman1').classList.add('animazione-sx');
+  // document.getElementById('air2').classList.add('animazione-centro');
+  // document.getElementById('black-eagle').classList.add('animazione-centro');
+  // document.getElementById('gunman2').classList.add('animazione-centro');
+  // document.getElementById('air3').classList.add('animazione-dx');
+  // document.getElementById('owl').classList.add('animazione-dx');
+  // document.getElementById('gunman3').classList.add('animazione-dx');
+}
+);
+document.getElementById('gunman2').addEventListener('click',
+function() {
+  document.getElementById('gunman2').classList.add('rotate');
+
+}
+);
